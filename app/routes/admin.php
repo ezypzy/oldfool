85703992d975dce13042c1dd795f04a174661a02
miketee <miketee@gmail.com>
2012-06-17 04:28:45 +0800

    Admin login page

--- app/routes/admin.php
+++ app/routes/admin.php
@@ -8,7 +8,11 @@
 $app->get('/adm/', 'admin');
 function admin() {
 	global $app, $db, $v;
+
 	// -- if logged in redirect to admin dashboard
+	$v['window_title'] = 'Boxfool - Admin Overlord Dashboard';
+	$v['adm_page'] = 'admin/login';
+	$app->render('admin/layout', $v);
 
 	// -- else redirect to login
 	$app->redirect(c::get('base_url') .'/adm/login');
@@ -21,19 +25,30 @@ function admin_login() {
 	global $app, $db, $v;
 	// -- if logged in then redirect to dashboard
 
-	$v['window_title'] = 'Boxfool - admin';
+	$v['window_title'] = 'Boxfool - Admin Overlord Login';
 	$v['adm_page'] = 'admin/login';
 
 	$app->render('admin/layout', $v);
 }
 
 
-$app->get('/adm/boxes/', 'admin_login');
+$app->get('/adm/dashboard/', 'admin_dashboard');
+function admin_dashboard() {
+	global $app, $db, $v;
+
+	$v['window_title'] = 'Boxfool - Admin Overlord Dashboard';
+	$v['adm_page'] = 'admin/dashboard';
+
+	$app->render('admin/layout', $v);
+}
+
+
+$app->get('/adm/boxes/', 'admin_boxes');
 function admin_boxes() {
 	global $app, $db, $v;
 	// -- if logged in then redirect to dashboard
 
-	$v['window_title'] = 'Boxfool - admin';
+	$v['window_title'] = 'Boxfool - Admin Overlord Login';
 	$v['adm_page'] = 'admin/boxes';
 
 	$app->render('admin/layout', $v);
