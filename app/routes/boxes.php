806ebe274d362bb57b03d33b95cc2fb3c854b698
J Shamsul Bahri <jibone@gmail.com>
2012-06-17 00:50:49 +0800

    .

--- app/routes/boxes.php
+++ app/routes/boxes.php
@@ -7,11 +7,22 @@
 
 $app->get('/boxes/', 'boxes_list');
 function boxes_list() {
-	
+	global $app, $v;
+	echo 'list of boxes';	
 }
 
+
+// --  page for boxes
 $app->get('/box/:name/', 'box_name_show');
-function box_name_show($name) {
+function box_name_show($name, $s = '') {
 	global $app, $v;
 	print_r($name);
+}
+
+// -- page for box subscribe
+$app->get('/box/:name/subscribe/', 'box_subscribe');
+function box_subscribe($name) {
+	global $app, $v;
+	print_r($name);
+	echo 'subscribe';
 }
