de8b36249a30507184c8376d4ce46320694bb475
Puaka Astro <puaka@puaka.(none)>
2012-06-17 01:15:46 +0800

    cache path changed

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Boxfool Web Bootstrap
- * 
+ *
  * bootstrap.php
  **/
 
@@ -17,25 +17,27 @@ require_once 'app/helpers/session.php';
 
 // -- init database connection
 
-// -- init session 
+// -- init session
 
 // -- init app
 $app = new Slim(array(
-	'view' => new RainView(),
-	'templates.path' => '../views'
+    'view' => new RainView(),
+    'templates.path' => '../views'
 ));
+
+// Change cache dir from "tmp" to "cache"
+RainTPL::$cache_dir =   'cache';
 
 // -- init the view data
 $v = array(
-	'base_url' => c::get('base_url'),
-	'window_title' => 'Boxfool of awesomeness'
+    'base_url'      =>  c::get('base_url'),
+    'window_title'  =>  'Boxfool of awesomeness'
 );
 
 $app->get('/', 'landing');
 function landing() {
-	global $v, $app;
-    $v['page']  =   'test';
-	$app->render('landing', $v);
+    global $v, $app;
+    $app->render('landing', $v);
 }
 
 require 'app/routes/boxes.php';
