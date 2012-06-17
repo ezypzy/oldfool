7b6b1f4279e353720fdc4ebdfa03f1fe39289004
J Shamsul (@jibone) <jibone@gmail.com>
2012-06-17 15:16:54 +0800

    restore deleted file

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Boxfool Web Bootstrap
- * 
+ *
  * bootstrap.php
  **/
 
@@ -16,29 +16,56 @@ require_once 'app/helpers/view.php';
 require_once 'app/helpers/session.php';
 
 // -- init database connection
+$db = new Database();
+$db->connect();
 
-// -- init session 
+// -- init session
 
 // -- init app
 $app = new Slim(array(
-	'view' => new RainView(),
-	'templates.path' => '../views'
+    'view'              =>  new RainView(),
+    'templates.path'    =>  '../views'
 ));
+
+// Change cache dir from "tmp" to "cache"
+RainTPL::$cache_dir =   'cache';
 
 // -- init the view data
 $v = array(
-	'base_url' => c::get('base_url'),
-    'page'          => 'blank',
-	'window_title' => 'Boxfool of awesomeness'
+    'base_url'      =>  c::get('base_url'),
+    'breadcrumb'    =>  'breadcrumb',
+    'page'          =>  'blank',
+    'window_title'  =>  'Boxfool of awesomeness'
 );
 
-$app->get('/', 'landing');
-function landing() {
-	global $v, $app;
-    //$v['page']  =   'test';
-	$app->render('landing', $v);
+function debug($args)
+{
+    echo '<pre>';
+    print_r($args);
+    echo '</pre>';
 }
 
+$base_template  =   'layout';
+$page_template  =   'page';
+
+$app->get('/', 'landing');
+function landing()
+{
+    global $v, $app,$base_template;
+    $v['page']    =   'landing';
+    $app->render($base_template, $v);
+}
+
+$app->get('/boxfools-of-:page', 'boxstarpage');
+function boxstarpage($page)
+{
+    global $v, $app, $page_template;
+    $v['page']  =   'boxfoolof'.$page;
+
+    $app->render($page_template, $v);
+}
+
+require 'app/routes/admin.php';
 require 'app/routes/boxes.php';
 
 $app->run();
