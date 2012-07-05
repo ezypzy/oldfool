a08932dc5b4888085bb14198701bf194bf6f58d0
J Shamsul Bahri <jibone@gmail.com>
2012-07-05 16:55:29 +0800

    contact form submission

--- app/routes/pages.php
+++ app/routes/pages.php
@@ -35,14 +35,34 @@ function page_faq()
 
 
 // -- routes to contact page
-$app->get('/contact/', 'page_contact');
+$app->map('/contact/', 'page_contact')->via('GET', 'POST');
 function page_contact()
 {
 	global $app, $v, $page_template;
 
 	$v = array_merge($v, array(
+		'form_success' => false,
 		'page' => 'page_contact'
 	));
+
+	if($app->request()->isPost()) {
+		$name = $app->request()->post('contact_name');
+		$email = $app->request()->post('contact_email');
+		$location = $app->request()->post('contact_location');
+		$comment = $app->request()->post('contact_comment'); 
+
+		//$email_to = "whatsup@boxfool.com";
+		$email_to = "jibone@gmail.com";
+		$email_subject = "Contact form";
+		$email_message = "Email from contact form, \r\n\r\n";
+		$email_message .= "Name: {$name}\r\n";
+		$email_message .= "Email: {$email}\r\n\r\n";
+		$email_message .= "location: {$location}\r\n";
+		$email_message .= "Message:\r\n";
+		$email_message .= $comment ."\r\n\r\n";
+		mail($email_to, $email_subject, $email_message);
+		$v['form_success'] = true;
+	}
 
 	$app->render($page_template, $v);
 }
